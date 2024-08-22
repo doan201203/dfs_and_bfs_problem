809a6a9b95a87b26ab8822b30c8ddc62c66be562
doan201203 <doan201203@gmail.com>
2024-08-23 01:24:25 +0700

    do

---
+++ B0/sol.cpp
@@ -0,0 +1,62 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+const int MAX_N = 3000;
+
+vector<vector<int>> adj(MAX_N);
+vector<int> vis(MAX_N);
+vector<int> closed(MAX_N);
+int nodes = 0;
+
+void dfs(int node) {
+  if (vis[node] || closed[node])
+    return;
+
+  nodes++;
+  vis[node] = true;
+
+  for (int u : adj[node]) {
+    if (!vis[u])
+      dfs(u);
+  }
+}
+
+int main() {
+  cin >> n >> m;
+
+  for (int i = 0; i < m; i++) {
+    int a, b;
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+
+  vector<int> ord(n);
+  for (int i = 0; i < n; i++)
+    cin >> ord[i];
+
+  dfs(1);
+
+  if (nodes == n) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
+
+  for (int i = 0; i < n - 1; i++) {
+    nodes = 0;
+    closed[ord[i]] = true;
+    fill(vis.begin(), vis.end(), false);
+
+    dfs(ord[n - 1]);
+
+    if (nodes == n - i - 1) {
+      cout << "YES" << "\n";
+    } else {
+      cout << "NO" << "\n";
+    }
+  }
+}
